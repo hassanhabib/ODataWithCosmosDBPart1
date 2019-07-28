98d090cdbf82a66e72e31dc1a680b01197fc0569
Hassan Habib <hassanhabib@live.com>
2019-07-27 23:29:30 -0700

    Add project files.

---
+++ Controllers/ItemsController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNet.OData;
+using Microsoft.AspNetCore.Mvc;
+using todo;
+using todo.Models;
+
+namespace quickstartcore.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Items")]
+    public class ItemsController : Controller
+    {
+        private readonly IDocumentDBRepository<Item> Respository;
+        public ItemsController(IDocumentDBRepository<Item> Respository)
+        {
+            this.Respository = Respository;
+        }
+
+        // GET: api/Items
+        [HttpGet]
+        [EnableQuery()]
+        public async Task<IEnumerable<Item>> Get()
+        {
+            return await Respository.GetItemsAsync(d => !d.Completed);
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,62 @@
+﻿using System.Linq;
+using Microsoft.AspNet.OData.Extensions;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using todo;
+
+namespace quickstartcore
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+            services.AddMvc();
+
+            services.AddSingleton<IDocumentDBRepository<todo.Models.Item>>(new DocumentDBRepository<todo.Models.Item>());
+            services.AddOData();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Item}/{action=Index}/{id?}");
+
+                routes.EnableDependencyInjection();
+                routes.Select().Filter().OrderBy().Expand();
+            });
+        }
+    }
+}
